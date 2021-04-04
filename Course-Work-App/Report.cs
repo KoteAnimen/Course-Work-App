fd66672fab3a0a8bb5c560a7d21fe3a5acddd18b
fil74 <fil74@DESKTOP-7JDCB6J>
2021-04-04 17:28:06 +0300

    Функция сохранения отчета полностью готова

--- Course-Work-App/Report.cs
+++ Course-Work-App/Report.cs
@@ -66,13 +66,12 @@ namespace Course_Work_App
 
         private void SaveReport_Click(object sender, EventArgs e)
         {
-            string path = string.Empty;
-            saveFileDialog1.DefaultExt = "xlsx";
+            string path = string.Empty;            
             using (SaveFileDialog saveDialog = new SaveFileDialog())
             {
                 if (saveDialog.ShowDialog() == DialogResult.OK)
                 {
-                    path = saveDialog.FileName;
+                    path = saveDialog.FileName + ".xlsx";
                     SaveTable(учетРаботыDataGridView, tableCount, path);                   
                 }
             }
@@ -85,20 +84,37 @@ namespace Course_Work_App
             ExcelPackage excelapp = new ExcelPackage();
             
             var worksheet = excelapp.Workbook.Worksheets.Add("Отчет");
+
+            worksheet.Cells[1, 1].Value = "Код операции";
+            worksheet.Cells[1, 2].Value = "Код номера";
+            worksheet.Cells[1, 3].Value = "Код клиента";
+            worksheet.Cells[1, 4].Value = "Дата заселения";
+            worksheet.Cells[1, 5].Value = "Дата выселения";
+            worksheet.Cells[1, 6].Value = "Стоимость проживания";
+            worksheet.Row(1).Height = 20;            
+            worksheet.Row(1).Style.HorizontalAlignment = ExcelHorizontalAlignment.Center;
+            worksheet.Row(1).Style.Font.Bold = true;
             
 
             for (int i = 1; i < dgw1.RowCount + 1; i++)
             {
                 for (int j = 1; j < dgw1.ColumnCount + 1; j++)
                 {
-                    worksheet.Cells[i, j].Value = Convert.ToString(dgw1.Rows[i - 1].Cells[j - 1].Value);
+                    worksheet.Cells[i + 1, j].Value = Convert.ToString(dgw1.Rows[i - 1].Cells[j - 1].Value);
+                    worksheet.Column(j).AutoFit();
                 }
             }
+            worksheet.Cells[dgw1.RowCount + 2, 1].Value = "Итоги месяца";
+            worksheet.Row(dgw1.RowCount + 2).Height = 20;
+            worksheet.Row(dgw1.RowCount + 2).Style.HorizontalAlignment = ExcelHorizontalAlignment.Center;
+            worksheet.Row(dgw1.RowCount + 2).Style.Font.Bold = true;
+
             for (int i = 1; i < dgw2.RowCount + 1; i++)
             {
                 for (int j = 1; j < dgw2.ColumnCount + 1; j++)
                 {
-                    worksheet.Cells[i + dgw1.RowCount, j].Value = dgw2.Rows[i - 1].Cells[j - 1].Value;
+                    worksheet.Cells[i + dgw1.RowCount + 2, j].Value = dgw2.Rows[i - 1].Cells[j - 1].Value;
+                    worksheet.Column(j).AutoFit();
                 }
             }
 
